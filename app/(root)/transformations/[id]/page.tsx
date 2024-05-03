117e5787b51208bd00dc54e5dd7d83a5ea40ec61
xiayulin123 <xiayulinsky@gmail.com>
2024-05-02 20:52:29 -0400

    initialize some routers

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Transformations = () => {
+  return (
+    <div>Transformations
+    
+    </div>
+  )
+}
+
+export default Transformations
---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateTransformation = () => {
+  return (
+    <div>UpdateTransformation
+    
+    </div>
+  )
+}
+
+export default UpdateTransformation
