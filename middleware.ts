b84bc1d74c30d4dd5e80adcad6bcaa6f72747666
xiayulin123 <xiayulinsky@gmail.com>
2024-05-07 21:31:01 -0400

    waiting finish debuging clerk

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
- 
-export default clerkMiddleware()
- 
+import { clerkMiddleware } from "@clerk/nextjs/server";
+
+export default clerkMiddleware();
+
 export const config = {
-  matcher: [
-    '/((?!.*\\..*|_next).*)', // Don't run middleware on static files
-    '/', // Run middleware on index page
-    '/(api|trpc)(.*)'], // Run middleware on API routes
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
